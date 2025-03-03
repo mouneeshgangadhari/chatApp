5eaa83ff04108b26ec6d40f7e76b37d6564318f4
Mouneesh Gangadhari <60066131+mouneeshgangadhari@users.noreply.github.com>
2025-03-03 18:40:04 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const MONGODB_URI="mongodb+srv://<UserName>:<Password>@cluster0.h0mmm.mongodb.net/chat_db";
-
   try {
-    const conn = await mongoose.connect(MONGODB_URI);
-
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
